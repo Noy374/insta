969922ca204337923cee1444a76623bee9bd6119
Noy374 <135060693+Noy374@users.noreply.github.com>
2023-07-26 14:29:56 +0300

    last commit

---
+++ src/main/java/com/example/insta/payload/reponse/MessageResponse.java
@@ -0,0 +1,10 @@
+package com.example.insta.payload.reponse;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MessageResponse {
+   private String message;
+}
